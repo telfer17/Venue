bbb36e6fafc4f3c5096ac26c9054936b8dadffe7
David Telfer <davietelfer@hotmail.com>
2018-12-04 18:51:43 +0000

    added event/location info

---
+++ src/components/venue_info/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import Zoom from 'react-reveal/Zoom';
+
+import icon_calendar from '../../resources/images/icons/calendar.png';
+import icon_location from '../../resources/images/icons/location.png';
+
+const VenueInfo = () => {
+  return (
+    <div className="bck_black">
+      <div className="center_wrapper">
+        <div className="vn_wrapper">
+
+          <Zoom duration={800}>
+          <div className="vn_item">
+            <div className="vn_outer">
+              <div className="vn_inner">
+                <div className="vn_icon_square bck_red"></div>
+                <div
+                  className="vn_icon"
+                  style={{
+                    background: `url(${icon_calendar})`
+                  }}
+                ></div>
+                <div className="vn_title">
+                  Event Date & Time
+                </div>
+                <div className="vn_desc">
+                  Sat 25 May 2019 @10pm
+                </div>
+              </div>
+            </div>
+          </div>
+          </Zoom>
+
+          <Zoom duration={800} delay={1200}>
+          <div className="vn_item">
+            <div className="vn_outer">
+              <div className="vn_inner">
+                <div className="vn_icon_square bck_yellow"></div>
+                <div
+                  className="vn_icon"
+                  style={{
+                    background: `url(${icon_calendar})`
+                  }}
+                ></div>
+                <div className="vn_title">
+                  Event Location
+                </div>
+                <div className="vn_desc">
+                  The Steelyard, London
+                </div>
+              </div>
+            </div>
+          </div>
+          </Zoom>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VenueInfo;
